6f579d483ab6111f0b734b3102753805b6f8197c
Indy Van Canegem <i.vancanegem@metamaze.eu>
2024-05-10 11:36:52 +0200

    feat: intiialize tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,22 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      accent: {
+        green: "#53EF62",
+        orange: "#F65403",
+        blue: "#2D9BD8",
+      },
+      primary: {
+        500: "#D9D9E1",
+        900: "#05192D",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
